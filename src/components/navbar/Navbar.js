c4ed4860ed42eec79ea5bc31e86cb12effaeb460
remiltan123 <regiremiltan2002@gmail.com>
2024-12-10 09:29:17 -0800

    Navbar edit

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -3,15 +3,16 @@ import { Link } from "react-scroll";
 import { FiMenu } from "react-icons/fi";
 import { MdClose } from "react-icons/md";
 import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
-import {logo} from "../../assets/index"
+import { logo } from "../../assets/index"
 import { navLinksdata } from '../../constants';
 
 const Navbar = () => {
-  const [showMenu, setShowMenu]=useState(false)
+  const [showMenu, setShowMenu] = useState(false)
   return (
     <div className="w-full h-24 sticky top-0 z-50 bg-bodyColor mx-auto flex justify-between items-center font-titleFont border-b-[1px] border-b-gray-600">
-      <div>
-        <img src={logo} alt="logo" />
+      <div className="flex items-center gap-3">
+        <img src={logo} alt="logo" className="w-16 h-16 object-cover rounded-full" />
+        <p>J.R.Remiltan</p>
       </div>
       <div>
         <ul className="hidden mdl:inline-flex items-center gap-6 lg:gap-10">
@@ -43,12 +44,8 @@ const Navbar = () => {
           <div className="w-[80%] h-screen overflow-scroll absolute top-0 left-0 bg-gray-900 p-4 scrollbar-hide">
             <div className="flex flex-col gap-8 py-2 relative">
               <div>
-                <img className="w-32" src={logo} alt="logo" />
-                <p className="text-sm text-gray-400 mt-2">
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                  Earum soluta perspiciatis molestias enim cum repellat, magnam
-                  exercitationem distinctio aliquid nam.
-                </p>
+                <img src={logo} alt="logo" className="w-16 h-16 object-cover rounded-full" />
+                <p>J.R.Remiltan</p>
               </div>
               <ul className="flex flex-col gap-4">
                 {navLinksdata.map((item) => (
@@ -75,15 +72,19 @@ const Navbar = () => {
                   Find me in
                 </h2>
                 <div className="flex gap-4">
-                  <span className="bannerIcon">
-                    <FaFacebookF />
-                  </span>
-                  <span className="bannerIcon">
-                    <FaTwitter />
-                  </span>
-                  <span className="bannerIcon">
-                    <FaLinkedinIn />
-                  </span>
+
+                  <a
+                    href="https://www.linkedin.com/in/regi-remiltan-re15" target="_blank" rel="noopener noreferrer">
+                    <span className="bannerIcon">
+                      <FaLinkedinIn />
+                    </span>
+                  </a>
+                  <a
+                    href="https://www.facebook.com/joesphregi.remiltan?mibextid=ZbWKwL" target="_blank" rel="noopener noreferrer">
+                    <span className="bannerIcon">
+                      <FaFacebookF />
+                    </span>
+                  </a>
                 </div>
               </div>
               <span
