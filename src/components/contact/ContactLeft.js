d91ee43758512aa98799675f6632692d58f68f2a
remiltan123 <regiremiltan2002@gmail.com>
2024-12-11 21:12:08 -0800

    Left Meassage Com

--- src/components/contact/ContactLeft.js
+++ src/components/contact/ContactLeft.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FaFacebookF, FaTwitter, FaLinkedinIn } from "react-icons/fa";
+import { FaFacebookF, FaGithub, FaLinkedinIn } from "react-icons/fa";
 import { contactImg } from "../../assets/index";
 
 const ContactLeft = () => {
@@ -11,33 +11,38 @@ const ContactLeft = () => {
         alt="contactImg"
       />
       <div className="flex flex-col gap-4">
-        <h3 className="text-3xl font-bold text-white">John Doe</h3>
-        <p className="text-lg font-normal text-gray-400">
-          MERN Stack Developer
+        <h3 className="text-3xl font-bold text-white">Regi Remi</h3>
+        <p className="text-lg font-normal text-white">
+          Full Stack Developer
         </p>
         <p className="text-base text-gray-400 tracking-wide">
-          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facilis
-          ipsam autem cumque, accusantium dicta odio.
+          Thanks for visiting my portfolio! If you have any questions or want to discuss a project, feel free to reach out. I'm open to new opportunities and collaborations. I look forward to hearing from you!
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Phone: <span className="text-lightText">+968 97859628</span>
+          Phone: <span className="text-lightText"><span className='font-bold text-white'>+94775353837</span></span>
         </p>
         <p className="text-base text-gray-400 flex items-center gap-2">
-          Email: <span className="text-lightText">noor.jsdivs@gmail.com</span>
+          Email: <span className="text-lightText"><span className='font-bold text-white'>regiremiltan2002@gmail.com</span></span>
         </p>
       </div>
       <div className="flex flex-col gap-4">
         <h2 className="text-base uppercase font-titleFont mb-4">Find me in</h2>
         <div className="flex gap-4">
-          <span className="bannerIcon">
-            <FaFacebookF />
-          </span>
-          <span className="bannerIcon">
-            <FaTwitter />
-          </span>
-          <span className="bannerIcon">
-            <FaLinkedinIn />
-          </span>
+        <a href="https://github.com/Remiltan123" target="blank">
+              <span className="bannerIcon">
+                <FaGithub />
+              </span>
+            </a>
+            <a href="https://www.linkedin.com/in/regi-remiltan-re15" target="blank">
+              <span className="bannerIcon">
+                <FaLinkedinIn />
+              </span>
+            </a>
+            <a href="https://www.facebook.com/joesphregi.remiltan?mibextid=ZbWKwL" target="blank">
+              <span className="bannerIcon">
+                <FaFacebookF />
+              </span>
+            </a>
         </div>
       </div>
     </div>
